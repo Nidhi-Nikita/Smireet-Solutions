bda658e106571a0a7af5cf1f2f9d966d56c66979
nidhivanjare <rnidhivanjare@gmail.com>
2021-03-14 21:34:41 +0530

    .

--- read_files.php
+++ read_files.php
@@ -7,13 +7,16 @@ if ($handle = opendir('./Videos')) {
 
         if ($entry != "." && $entry != ".." && $entry != ".DS_Store") {
 
-            $newArray[] = $entry;
+            $newArray[] =  "Videos/" . $entry;
         }
     }
 
     closedir($handle);
 }
 
+
+
 // for ($i = 0; $i < count($newArray); $i++) {
+//     // $newArray[$i] = "Videos/" . $newArray[$i];
 //     echo $newArray[$i] . "\n";
 // }
